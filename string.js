d7bbce70289363f27946a24d774adf42e86d2b7c
Md Nozib Ud Dowla <nozibuddowla@gmail.com>
2025-08-01 11:23:46 +0600

    first commit

---
+++ string.js
@@ -0,0 +1,16 @@
+/*
+Task-1:
+Count how many times a string has the letter a
+*/ 
+
+// let str = "JavaScript is a powerful and amazing language.";
+// let count = 0;
+
+// for (const element of str.toLowerCase()) {
+//     if (element === 'a') {
+//         count = count + 1;
+//     }
+// }
+
+// console.log(`Number of 'a': ${count}`);
+
