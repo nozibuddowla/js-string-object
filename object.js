0becc7db30e67e79f77ab10081d3edc33413e567
Md Nozib Ud Dowla <nozibuddowla@gmail.com>
2025-08-02 14:22:58 +0600

    add fourth commit

--- object.js
+++ object.js
@@ -32,15 +32,71 @@ For this object below add a property named passenger capacity with value 5
 Task-3
 Display the physics marks as output.
 */
-const student = {
-    name: "Hamim Sakep",
-    id: 5421,
-    physics: {
-        subject: "HSC Physics",
-        author: "Shahjahan Tapan",
-        marks: 30
-    }
+// const student = {
+//     name: "Hamim Sakep",
+//     id: 5421,
+//     physics: {
+//         subject: "HSC Physics",
+//         author: "Shahjahan Tapan",
+//         marks: 30
+//     }
+// };
+
+// console.log(student.physics.marks);
+
+
+/*
+Task-4
+Count the number of properties.
+
+Input:
+let student = {
+    name: 'Ariana Grande',
+    age: 21,
+    city: 'Gaibandha',
+    isStudent: true
 };
 
-console.log(student.physics.marks);
+Output:
+4
+*/
+let student = {
+    name: 'Ariana Grande',
+    age: 21,
+    city: 'Gaibandha',
+    isStudent: true
+};
 
+console.log(`Number of properties: ${(Object.keys(student)).length}`);
+
+/* 
+Task-5 (Hard)
+Loop through an object and print the key-value pairs with their types
+
+Input:
+
+let myObject = {
+    name: 'John Doe',
+    age: 25,
+    city: 'Example City',
+    isStudent: true
+};
+
+Output:
+key: name | type:  string
+key: age | type:  number
+key: city | type:  string
+key: isStudent | type:  boolean
+
+*/
+
+let myObject = {
+    name: 'John Doe',
+    age: 25,
+    city: 'Example City',
+    isStudent: true
+};
+
+for (const key in myObject) {
+    console.log(`Key: ${key} | Type: ${ typeof myObject[key]}`);
+}
--- string.js
+++ string.js
@@ -35,13 +35,39 @@ Count how many times a string has the letter a or A
 Task-3:
 Check whether a string contains all the vowels a, e, i, o, u
 */ 
-let str = "Education is a powerful tool.";
-str = str.toLowerCase();
+// let str = "Education is a powerful tool.";
+// str = str.toLowerCase();
 
-if (str.includes('a') && str.includes('e') && str.includes('i') && str.includes('o') && str.includes('u')) {
-    console.log("The string contains all the vowels.");
+// if (str.includes('a') && str.includes('e') && str.includes('i') && str.includes('o') && str.includes('u')) {
+//     console.log("The string contains all the vowels.");
     
-} else {
-    console.log("The string does not contain all the vowels.");
+// } else {
+//     console.log("The string does not contain all the vowels.");
     
+// }
+
+/*
+Task-4:
+If a given string has either x, replace x by y. if the given string has X, replace it by Y.
+*/ 
+
+// let str = "Xerox and xylophone are examples.";
+
+// if (str.includes("x") || str.includes("X")) {
+//     let result = str.replace(/x/g, "y").replace(/X/g, "Y");
+//     console.log(result);
+// }
+
+/*
+Task-5:
+Capitalize Every first Letter of each word in a String
+*/
+
+let str = "the quick brown fox jumps over the lazy dog";
+str = str.split(" ")
+
+for (let index = 0; index < str.length; index++) {
+    str[index] = str[index][0].toUpperCase() + str[index].slice(1);
 }
+
+console.log(str.join(" "));
